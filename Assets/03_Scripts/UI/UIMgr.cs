cb17ab336cd87f88424dd843afd76b0242561c2b
hubysh <design.ysh@gmail.com>
2016-05-11 18:07:47 +0900

    add - Scene 병합 스크립트 추가

---
+++ Assets/03_Scripts/UI/UIMgr.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIMgr : MonoBehaviour
+{
+	public void OnClickStartBtn()
+	{
+		Debug.Log("Click Button");
+		Application.LoadLevel("scnSphere");
+		Application.LoadLevelAdditive("scnCylinder");
+	
+	}
+}
